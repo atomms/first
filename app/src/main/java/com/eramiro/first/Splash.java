b57b4dbe995b1a3398eca3aaa895484ebb7b471a
eramiro <atomms@gmail.com>
2024-10-28 14:02:25 +0100

    shake from D ready

--- app/src/main/java/com/eramiro/first/Splash.java
+++ app/src/main/java/com/eramiro/first/Splash.java
@@ -38,7 +38,7 @@ public class Splash extends AppCompatActivity {
 
         //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
         // startAnimation lo aplicamos al imageview del logo
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.shake);
         thunder.startAnimation(myanim);
 
 
@@ -47,7 +47,6 @@ public class Splash extends AppCompatActivity {
 
         Glide.with(this)
               .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
-//                .load(R.drawable.girl)
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
@@ -69,9 +68,6 @@ public class Splash extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-
-
-
             }
         }, 5000);
 
