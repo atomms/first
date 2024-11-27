422f0e3516516d933c283c7ed49c7bb3d67e1d84
eramiro <atomms@gmail.com>
2024-11-27 13:05:34 +0100

    texto color in splash

--- app/src/main/java/com/eramiro/first/Login.java
+++ app/src/main/java/com/eramiro/first/Login.java
@@ -49,6 +49,7 @@ public class Login extends AppCompatActivity {
         ImageView mGirl = findViewById(R.id.girl);
 //        ImageView mLogo = findViewById(R.id.logo);
 
+
 //      Intenta varias cargas hasta que la imagen se muestre
 //        loadImage(mGirl,"https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80", 2, 0);
 
